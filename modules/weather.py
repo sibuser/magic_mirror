11f65fa9407dd90ff3284151192b5e2e1cbfb100
Alexey Ulyanov <sibuser.nsk@gmail.com>
2018-04-15 23:44:41 +0200

    Restored original icons.

--- modules/weather.py
+++ modules/weather.py
@@ -89,7 +89,7 @@ class Weather(BaseModule):
     @property
     def condition(self):
         icon_name = self.icon_mapping[str(self.weather_data['weather'][0]['id'])]['icon']
-        icon_name = 'cloudy'
+        icon_name = '056-cloudy'
         surface = pygame.image.load(os.path.join('resources', 'icons', '%s.png' % icon_name))
         surface = pygame.transform.scale(surface, (int(self.width * 0.07), int(self.width * 0.07)))
         position = surface.get_rect(left=self.width / 8, top=self.height * 0.04)
