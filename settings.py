67f340c89f69d94387d665ccab5e94a670c8b1fa
Alexey Ulyanov <sibuser.nsk@gmail.com>
2018-04-15 22:51:43 +0200

    Added more colors.

--- settings.py
+++ settings.py
@@ -39,10 +39,22 @@ KEY_ESCAPE = 27
 KEY_WINDOW_X = 12
 
 COLORS = {
-    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    'aqua': (0, 255, 255),
+    'blue': (0, 0, 255),
+    'fuchsia': (255, 0, 255),
     'gray': (128, 128, 128),
+    'green': (0, 128, 0),
+    'lime': (0, 255, 0),
+    'maroon': (128, 0, 0),
+    'navy_blue': (0, 0, 128),
+    'olive': (128, 128, 0),
+    'purple': (128, 0, 128),
     'red': (255, 0, 0),
-    'black': (0, 0, 0)
+    'silver': (192, 192, 192),
+    'teal': (0, 128, 128),
+    'white': (255, 255, 255),
+    'yellow': (255, 255, 0),
 }
 
 FONTS = {
@@ -50,5 +62,3 @@ FONTS = {
     'light': path.join("resources", "font", "font-light.ttf"),
     'regular': path.join("resources", "font", "font-regular.ttf")
 }
-
-
