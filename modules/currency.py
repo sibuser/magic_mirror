eeceb14cd3c1320e9be110c0023bf96d4318ce8b
Alexey Ulyanov <sibuser.nsk@gmail.com>
2018-04-18 00:27:28 +0200

    Moved exchange rates down.

--- modules/currency.py
+++ modules/currency.py
@@ -25,19 +25,19 @@ class Currency(BaseModule):
 
     def show_rate(self):
         surface = self.font('regular', 0.025).render('SEK/RUB', True, self.color)
-        position = surface.get_rect(left=self.width / 24, top=300)
+        position = surface.get_rect(left=self.width / 24, top=self.height * 0.5)
         self.new_data.append((surface, position))
 
         rate = CurrencyConverter().convert(1, 'SEK', 'RUB')
         surface = self.font('regular', 0.025).render(str(round(rate, 2)), True, self.color)
-        position = surface.get_rect(left=self.width / 7, top=300)
+        position = surface.get_rect(left=self.width / 7, top=self.height * 0.5)
         self.new_data.append((surface, position))
 
         surface = self.font('regular', 0.025).render('USD/RUB', True, self.color)
-        position = surface.get_rect(left=self.width / 24, top=320)
+        position = surface.get_rect(left=self.width / 24, top=self.height * 0.55)
         self.new_data.append((surface, position))
 
         rate = CurrencyConverter().convert(1, 'USD', 'RUB')
         surface = self.font('regular', 0.025).render(str(round(rate, 2)), True, self.color)
-        position = surface.get_rect(left=self.width / 7, top=320)
+        position = surface.get_rect(left=self.width / 7, top=self.height * 0.55)
         self.new_data.append((surface, position))
