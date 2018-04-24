6e9696fb0a19d6add168e04cefe1f66cacac425b
Alexey Ulyanov <sibuser.nsk@gmail.com>
2018-04-24 21:01:23 +0200

    Removed logs.

--- launch_mirror.sh
+++ launch_mirror.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-echo "Activating environment." > /var/log/magic_init.log
-source /home/pi/.virtualenv/magic/bin/activate
-
-echo "Entering app folder." >> /var/log/magic_init.log
 cd /home/pi/dev/magic_mirror
-
-echo "Setting display." >> /var/log/magic_init.log
+source /home/pi/.virtualenv/magic/bin/activate
 export DISPLAY=:0
-
-echo "Launching magic mirror." >> /var/log/magic_init.log
 ./main.py -f
