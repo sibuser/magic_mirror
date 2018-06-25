d76810c482e77d99974ce0e9a3b1fad0f6dcf63e
Alexey Ulyanov <sibuser.nsk@gmail.com>
2018-06-25 20:00:13 +0200

    Show ip address with using netifaces. More readable code.
    Previous solution shows 127.0.0.1

--- modules/system_info.py
+++ modules/system_info.py
@@ -1,10 +1,11 @@
 import logging
+import netifaces as ni
 import socket
 
 from threading import Thread
 
 from modules.base import BaseModule
-from settings import SYS_INFO_UPDATE_DELAY
+from settings import SYS_INFO_UPDATE_DELAY, IP_INTERFACE
 
 
 class SystemInfo(BaseModule):
@@ -27,8 +28,8 @@ class SystemInfo(BaseModule):
         logging.info('Stopped %s...' % self.__class__.__name__)
 
     def show_ip(self):
-        current_ip = socket.gethostbyname(socket.gethostname())
-
-        surface = self.font('regular', self.ip_scale).render(current_ip, True, self.color)
-        position = surface.get_rect(left=self.width * 0.01, top=self.height * 0.98)
-        self.new_data.append((surface, position))
+        if socket.gethostname() == 'mirror':
+            ip = ni.ifaddresses(IP_INTERFACE)[ni.AF_INET][0]['addr']
+            surface = self.font('regular', self.ip_scale).render(ip, True, self.color)
+            position = surface.get_rect(left=self.width * 0.01, top=self.height * 0.98)
+            self.new_data.append((surface, position))
